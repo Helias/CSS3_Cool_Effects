8d838399ba8d50d2e703e643666d0c035e47726c
Helias <stefano520@hotmail.it>
2014-05-15 18:29:21 +0200

    Updated the download link and the effect appear

--- website/effects/radius.php
+++ website/effects/radius.php
@@ -44,7 +44,7 @@
               </a>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="https://github.com/Helias/CSS3_Cool_Effects/archive/master.zip"><i class="fa fa-download"></i>&nbsp;Doanload All Effects</a></li>
+              <li><a href="https://raw.githubusercontent.com/Helias/CSS3_Cool_Effects/master/effects.css"><i class="fa fa-download"></i>&nbsp;Doanload All Effects</a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
