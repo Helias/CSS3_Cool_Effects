7b8cfc474539314a70d868d9292264d8ab7a9b0c
Helias <stefano520@hotmail.it>
2014-05-21 20:27:34 +0200

    Joined Radius and radius2, created another effect based on "show" effect

--- website/effects/radius.php
+++ website/effects/radius.php
@@ -124,6 +124,74 @@
 </textarea>
         </div>
       </div>
+      <div class="container">
+      <br><br>
+      <div class="row">
+        <div class="col-md-4 hidden-xs">
+          <div class="hidden-sm" style="margin-top: 120px;"></div>
+          <div class="radius2" style="border: 2px solid #33a6e7; width: 425px; text-align: center;">
+            <font style="padding: 10px;" size="7px" color="#33a6e7">CSS3 Cool Effects</font>
+          </div>
+        </div>
+        <div class="col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 hidden-xs">
+          <font size="8">HTML:</font>
+          <textarea readonly="readonly" cols="45" rows="6">
+
+  <div class="radius2" style="border: 2px solid #33a6e7;">
+    CSS3 Cool Effects
+  </div>
+</textarea>
+          <br>
+          <font size="8">CSS:</font>
+          <textarea readonly="readonly" cols="45" rows="5">
+
+  /* Border-Radius */
+  .radius2 { border-radius: 5em 0px; }
+  .radius2:hover { border-radius: 0px 5em; }
+
+  .radius2, .radius2:hover {
+    -webkit-transition: all 1s ease-in-out;
+    -moz-transition: all 1s ease-in-out;
+    -o-transition: all 1s ease-in-out;
+    -ms-transition: all 1s ease-in-out;
+    transition: all 1s ease-in-out;
+  }
+</textarea>
+        </div>
+        <div class="visible-xs col-xs-12">
+          <div class="radius2" style="border: 2px solid #33a6e7; width: 425px; text-align: center;">
+            <font style="padding: 10px;" size="6px" color="#33a6e7">CSS3 Cool Effects</font>
+          </div>
+          <br><br>
+        </div>
+        <div class="visible-xs col-xs-12">
+          <font size="6">HTML:</font>
+          <br><br>
+          <textarea readonly="readonly" cols="45" rows="6">
+
+  <div class="radius2" style="border: 2px solid #33a6e7;">
+    CSS3 Cool Effects
+  </div>
+</textarea>
+          <br>
+          <font size="6">CSS:</font>
+          <br><br>
+          <textarea readonly="readonly" cols="45" rows="5">
+
+  /* Border-Radius */
+  .radius2 { border-radius: 5em 0px; }
+  .radius2:hover { border-radius: 0px 5em; }
+
+  .radius2, .radius2:hover {
+    -webkit-transition: all 1s ease-in-out;
+    -moz-transition: all 1s ease-in-out;
+    -o-transition: all 1s ease-in-out;
+    -ms-transition: all 1s ease-in-out;
+    transition: all 1s ease-in-out;
+  }
+</textarea>
+        </div>
+      </div>
       <?php include "../footer.php"; ?>
     </div>
 
