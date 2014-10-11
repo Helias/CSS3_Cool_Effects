fc8ecfb038d3bed9fdc7fd4a42be38007b5ef3b6
Helias <stefano520@hotmail.it>
2014-10-11 18:46:57 +0200

    Updated header and effects.css

--- website/header.php
+++ website/header.php
@@ -9,7 +9,7 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/font-awesome.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
-    <link href="../../effects.css" rel="stylesheet">
+    <link href="../css/effects.css" rel="stylesheet">
     <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
